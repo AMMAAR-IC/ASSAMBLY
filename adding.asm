f88adacfff3eceeb7edb277fd729486fc5125e19
AMMAAR-IC <the.ammaar.ic@gmail.com>
2025-05-15 01:07:59 +0530

    1

---
+++ adding.asm
@@ -0,0 +1,16 @@
+.model small
+.data
+num1 db 10h
+num2 db 20h
+res db ?
+
+.code 
+start:mov ax,@data
+      mov ds,ax
+      mov ax,0000h
+      
+      mov al,num1
+      mov ah,num2
+      add al,ah
+      mov res,al
+end start
