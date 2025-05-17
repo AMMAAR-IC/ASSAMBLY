373cf313ead08b70807959c65562a7fff6855fbf
AMMAAR-IC <the.ammaar.bakshi@gmail.com>
2025-05-18 00:19:05 +0530

    9

--- PALINDROME.asm
+++ PALINDROME.asm
@@ -2,36 +2,75 @@ ASSUME DS:DATA
 ASSUME CS:CODE
 
 DATA SEGMENT
-SSTRING DB "AMMAAR$"
-DSTRING DB 30 DUP (?)
+    SSTRING DB "AMMAA$", 0
+    DSTRING DB 30 DUP (?)
+    YES DB "PALINDROME$"
+    NO DB "NOT A PALINDROME$"
 DATA ENDS
 
 CODE SEGMENT
 START:
-    MOV AX,DATA
-    MOV DS,AX
-    MOV AX,0000H
+    MOV AX, DATA
+    MOV DS, AX
 
-    LEA SI,SSTRING
-    LEA DI,DSTRING
-COUNT:
-    INC SI
-    CMP [SI],"$"    
-    INC AL
-    JNE COUNRT
- FN:
-    DEC AL
-    MOV BL,[SI+AL]
-    MOV [DI],BL
+    ; Initialize pointers
+    LEA SI, SSTRING
+    LEA DI, DSTRING
+
+    ; Find the length of the string
+    MOV CX, 0
+FIND_LENGTH:
+    MOV AL, [SI + CX]
+    CMP AL, '$'
+    JE LENGTH_FOUND
+    INC CX
+    JMP FIND_LENGTH
+
+LENGTH_FOUND:
+    MOV BX, CX          ; BX = length of the string
+    DEC BX              ; Adjust for 0-based index
+
+    ; Reverse the string
+    LEA SI, SSTRING
+    LEA DI, DSTRING
+REVERSE_LOOP:
+    MOV AL, [SI + BX]
+    MOV [DI], AL
     INC DI
-    CMP AL,00H
-    JNE FN
-    MOV [DI],"$"
-    LEA SI,SSTRING
-    LEA DI,DSTRING
-COMPARE:
-    MOV AL,[SI]
-    MOV BL,[DI]
-    CMP AL,BL
-    JE COMPARE
-    
+    DEC BX
+    CMP BX, -1
+    JG REVERSE_LOOP
+
+    ; Null-terminate the reversed string
+    MOV BYTE PTR [DI], '$'
+
+    ; Compare original and reversed strings
+    LEA SI, SSTRING
+    LEA DI, DSTRING
+COMPARE_LOOP:
+    MOV AL, [SI]
+    CMP AL, '$'
+    JE IS_PALINDROME
+    MOV BL, [DI]
+    CMP AL, BL
+    JNE NOT_PALINDROME
+    INC SI
+    INC DI
+    JMP COMPARE_LOOP
+
+IS_PALINDROME:
+    LEA DX, YES
+    JMP DISPLAY_RESULT
+
+NOT_PALINDROME:
+    LEA DX, NO
+
+DISPLAY_RESULT:
+    MOV AH, 09H
+    INT 21H
+
+    ; Terminate program
+    MOV AH, 4CH
+    INT 21H
+CODE ENDS
+END START
