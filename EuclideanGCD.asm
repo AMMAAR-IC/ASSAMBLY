5050eacfa69ae4eb2e1b0d9b7bb6f357a201a2a7
AMMAAR BAKSHI <the.ammaar.ic@gmail.com>
2025-05-31 22:37:22 +0530

    26

---
+++ EuclideanGCD.asm
@@ -0,0 +1,31 @@
+.model small
+.stack 100h
+
+.data
+    a dw 48
+    b dw 18
+    gcd dw ?
+
+.code
+main:
+    mov ax, @data
+    mov ds, ax
+
+    mov ax, a
+    mov bx, b
+
+gcd_loop:
+    cmp bx, 0
+    je store
+    mov dx, 0
+    div bx
+    mov ax, bx
+    mov bx, dx
+    jmp gcd_loop
+
+store:
+    mov gcd, ax
+
+    mov ah, 4Ch
+    int 21h
+end main
